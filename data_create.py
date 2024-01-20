ae74a2a7866320c2241e1d989af3b1e88a3b46a8
Anna Oleksevich <oleksanna@mail.ru>
2024-01-20 14:38:15 +0800

    Дополнили data_create.py

---
+++ data_create.py
@@ -0,0 +1,20 @@
+def name_data():
+    name = input('Введите Ваше имя: ')
+    print('Очень красивое имя!')
+    return name
+
+
+def surname_data():
+    surname = input('Введите Вашу фамилию: ')
+    return surname
+
+
+def phone_data():
+    phone = input('Введите Ваш номер телефона: ')
+    return phone
+
+
+def address_data():
+    address = input('Введите Ваш адрес: ')
+    return address
+
